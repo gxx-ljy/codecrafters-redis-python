06e8416549afcfbe09b1f4da35ac80aa1d614aac
蓝净云 <gxx_ljy@outlook.com>
2026-04-02 16:41:01 +0800

    hardcode a +PONG\r\n response

--- app/main.py
+++ app/main.py
@@ -7,9 +7,10 @@ def main():
 
     # Uncomment the code below to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 6379))
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
