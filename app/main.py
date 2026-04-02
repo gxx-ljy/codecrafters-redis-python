74a05d075558efda78d196d92c81030c6f844cb4
蓝净云 <gxx_ljy@outlook.com>
2026-04-02 17:48:52 +0800

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,25 +1,32 @@
 import socket  # noqa: F401
+import threading
 
+def handle_client(conn):
+    while True:
+        data = conn.recv(1024)
+        command = data.decode("utf-8")
+        print(f"Received command: {command}")
+        if not data:
+            break
+        conn.sendall(b"+PONG\r\n")
+
+    conn.close()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment the code below to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket = socket.create_server(("localhost", 6379)) # windows
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 6379)) # windows
 
-    connection, _ = server_socket.accept()
-    while True:
-        BUFFER_SIZE_BYTES = 1024
-        data = connection.recv(BUFFER_SIZE_BYTES)
-        command = data.decode("utf-8")
-        print(f"Received command: {command}")
-
-        connection.sendall(b"+PONG\r\n")
-
-    connection.close()
+    try:
+        while True:
+            connection, _ = server_socket.accept()  # wait for client
+            threading.Thread(target=handle_client, args=(connection,)).start()
+    except KeyboardInterrupt:
+        print("Shutting down server...")
+    finally:
+        server_socket.close()
 
 if __name__ == "__main__":
     main()
