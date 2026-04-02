ea81ec89977951ba1beb8ac593eac70f3953404e
蓝净云 <gxx_ljy@outlook.com>
2026-04-02 17:27:57 +0800

    Respond to multiple PINGS

--- app/main.py
+++ app/main.py
@@ -9,9 +9,17 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     # server_socket = socket.create_server(("localhost", 6379)) # windows
+
+    connection, _ = server_socket.accept()
     while True:
-        connection, t = server_socket.accept()
+        BUFFER_SIZE_BYTES = 1024
+        data = connection.recv(BUFFER_SIZE_BYTES)
+        command = data.decode("utf-8")
+        print(f"Received command: {command}")
+
         connection.sendall(b"+PONG\r\n")
+
+    connection.close()
 
 if __name__ == "__main__":
     main()
